a5452b4397728e537d644fd53ce0440c68211fce
Mario Segura <mariosegura0904@gmail.com>
2021-08-12 18:23:00 -0500

    Hola mundo MVC API

---
+++ cursomvcapi/Controllers/AccessController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using cursomvcapi.Models.WS;
+
+namespace cursomvcapi.Controllers
+{
+    public class AccessController : ApiController
+    {
+        [HttpGet]
+        public Reply HelloWorld()
+        {
+            Reply oR = new Reply();
+            oR.result = 1;
+            oR.message = "Hello world MVC Api";
+            return oR;
+        }
+    }
+}
